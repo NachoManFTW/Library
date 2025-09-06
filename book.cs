c57f3237a4dedf3a43836ecde18f4f4b005f0a69
Daniel Pererva <156557819+NachoManFTW@users.noreply.github.com>
2025-09-06 11:19:46 -0500

    Revert "organization"
    This reverts commit 3b7b1171e32e67bcb2f1cedad65d5a95a38ccf4b.

---
+++ book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySystem
+{
+    //Struct for each book added, each book has a title, author, genre,
+    //short description, ISBN number, overall rating, cost in USD, and a total number of pages
+    struct Book
+    {
+        string title;
+        string author;
+        string genre;
+        string description;
+
+        string ISBN;
+        string rating;
+        double cost;
+        int numberOfPages;
+
+        Book(string TITLE, string AUTHOR, string GENRE, string DESCRIPTION, string ISBN_NUMBER, string RATING, double COST, int NUMBER_OF_PAGES)
+        {
+            title =         TITLE;
+            author =        AUTHOR;
+            genre =         GENRE;
+            description =   DESCRIPTION;
+            ISBN =          ISBN_NUMBER;
+            rating =        RATING;
+            cost =          COST;
+            numberOfPages = NUMBER_OF_PAGES;
+
+        }
+    }
+}
